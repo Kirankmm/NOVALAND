ecc7a53b460b4ab0c2c2235402e40625406bc5bd
Kirankm <kirnkkm10@gmail.com>
2025-07-19 12:43:10 +0530

    Add files via upload

---
+++ src/components/SignInComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SignIn } from '@clerk/clerk-react'
+function SignInComponent() {
+  return (
+    <div>
+      <div>
+        <SignIn />
+      </div>
+    </div>
+  )
+}
+
+export default SignInComponent;
